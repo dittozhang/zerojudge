0049d23cd9428b5cc6f2ba855564df2b9c1f6480
b43f1FZh <dittozhang09@gamil.com>
2018-08-18 22:25:27 +0800

    commit a004.py

---
+++ a004.py
@@ -0,0 +1,13 @@
+#a004 文文的求婚
+
+while True:
+    try:
+        year = int(input())
+
+        if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+            print("閏年")
+        else:
+            print("平年")
+
+    except EOFError:
+        break
