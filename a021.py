07b31027c693abf904fa7c34a1583cff8f58ba3c
b43f1FZh <dittozhang09@gamil.com>
2018-08-22 23:08:31 +0800

    first commit a021

---
+++ a021.py
@@ -0,0 +1,18 @@
+#a021 大數運算
+#未完成
+from decimal import Decimal
+
+while True:
+    try:
+        num1, s, num2 = input().split()
+        num1, num2 = Decimal(num1), Decimal(num2)
+        if s == '+':
+            print(num1 + num2)
+        elif s == '-':
+            print(num1 - num2)
+        elif s == '*':  #不精確
+            print(num1 * num2)
+        else:
+            print(num1 // num2)
+    except EOFError:
+        break
