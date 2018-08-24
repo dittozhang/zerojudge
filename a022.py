acf8ed55eb446bfd309b0d853e6758b1d21136ef
b43f1FZh <dittozhang09@gamil.com>
2018-08-25 00:11:47 +0800

    firs commit a013

---
+++ a022.py
@@ -0,0 +1,18 @@
+#a022 迴文
+
+while  True:
+    try:
+        s = input()
+        s1 = s[:len(s)//2]
+        if len(s) % 2 == 0:
+            s2 = s[len(s)//2:]
+        else:
+            s2 = s[len(s)//2+1:]
+        s2 = s2[::-1]    #slice to reverse s2
+        if s1 == s2:
+            print('yes')
+        else:
+            print('no')
+
+    except EOFError:
+        break
